7f6ce3ef285597c8a02a145d7ef3f7e88ad3a724
ohjenssen <oskar_jenssen@hotmail.com>
2023-06-16 14:35:16 +0200

    Updated link to https

--- src/js/api/constants/index.mjs
+++ src/js/api/constants/index.mjs
@@ -1,4 +1,4 @@
-export const url_base = 'http://rugaard.oskarjenssen.com/wp-json/wp/v2/posts';
+export const url_base = 'https://rugaard.oskarjenssen.com/wp-json/wp/v2/posts';
 
 export const categories = {
         forside: '?categories=9&_embed',
