ef1c2a15a87f40410aa35b8c1e3fdd4e5f3ec3fc
ohjenssen <oskar_jenssen@hotmail.com>
2023-06-17 16:36:10 +0200

    Conditional rendering for 'maerkesager'

--- src/js/components/maerkesager.mjs
+++ src/js/components/maerkesager.mjs
@@ -6,21 +6,23 @@ export function maerkesagerComponent(post){
         <div class="section-content d-flex justify-content-center">
             <div class="card">
                 <div class="image-container">
-                    <img src="/assets/images/den-gamle-by-national.jpg" class="card-img-top" alt="...">
+                    <img src="https://cm.michaelrugaard.dk/wp-content/uploads/2023/06/Det_Konservative_Folkeparti.png" class="card-img-top" alt="...">
                 </div>
                 <div class="card-body">
                 <h5 class="card-title"></h5>
                 <p class="card-text"></p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <a href="#" class="btn btn-primary">Læs mer</a>
                 </div>
             </div>
         </div>
     `;
 
     container.querySelector('.card-title').innerText = post.title.rendered;
-    container.querySelector('.card-img-top').src = post._embedded['wp:featuredmedia'][0]['source_url'];
     container.querySelector('.card-text').innerHTML = post.excerpt.rendered;
     container.querySelector('.btn').href=`/pages/maerkesag/?id=${post.id}`;
+    if (post._embedded['wp:featuredmedia']?.[0]?.source_url) {
+        container.querySelector('.card-img-top').src = post._embedded['wp:featuredmedia'][0]['source_url'];
+    }
 
     return container;
 }
@@ -36,15 +38,17 @@ export function specificMaerkesag(post){
             <div class="hero-text text-black"></div>
         </div>
         <div class="col-md-6 hero-img-container">
-            <img class="hero-img" src="" alt="">
+            <img class="hero-img" src="https://cm.michaelrugaard.dk/wp-content/uploads/2023/06/Det_Konservative_Folkeparti.png" alt="Konservative folkeparti">
         </div>
     </div>
     `;
 
     container.querySelector('.hero-title').innerText = post.title.rendered;
     container.querySelector('.hero-text').innerHTML = post.excerpt.rendered;
-    container.querySelector('.hero-img').src = post._embedded['wp:featuredmedia'][0]['source_url'];
-    container.querySelector('.hero-img').alt = post._embedded['wp:featuredmedia'][0]['alt_text'];
+    if (post._embedded['wp:featuredmedia']?.[0]?.source_url) {
+        container.querySelector('.hero-img').src = post._embedded['wp:featuredmedia'][0]['source_url'];
+        container.querySelector('.hero-img').alt = post._embedded['wp:featuredmedia'][0]['alt_text'];
+    }
 
     const contentContainer = document.querySelector('#maerkesag-content');
     contentContainer.innerHTML = post.content.rendered;
