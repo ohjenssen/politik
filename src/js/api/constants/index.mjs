4a3289a0bea4866d31993bb5c346d2b03c97beb6
ohjenssen <oskar_jenssen@hotmail.com>
2023-06-17 16:35:50 +0200

    Updated constants

--- src/js/api/constants/index.mjs
+++ src/js/api/constants/index.mjs
@@ -1,9 +1,9 @@
-export const url_base = 'https://rugaard.oskarjenssen.com/wp-json/wp/v2/posts';
+export const url_base = 'https://cm.michaelrugaard.dk/wp-json/wp/v2/posts';
 
 export const categories = {
-        forside: '?categories=9&_embed',
+        forside: '?categories=3&_embed',
         footer: '?categories=4',
         maerkesager: '?categories=1&_embed',
         politik: '?categories=2',
-        om: '?categories=3&_embed'
+        om: '?categories=5&_embed'
 }
